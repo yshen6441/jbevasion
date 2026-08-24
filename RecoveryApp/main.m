3abec64e36287d684401a9d8e577c0c6516c684f
yshen6441 <yshen6441@users.noreply.github.com>
2026-08-24 14:04:00 +0000

    fix: add persona entitlements and NSLog logging to RecoveryApp for debugging

--- RecoveryApp/main.m
+++ RecoveryApp/main.m
@@ -2,6 +2,8 @@
 #import <spawn.h>
 #import <sys/wait.h>
 #import <dlfcn.h>
+#import <string.h>
+#import <unistd.h>
 
 extern char **environ;
 
@@ -17,22 +19,38 @@ static void load_persona_api(void) {
         my_persona_set = dlsym(h, "posix_spawnattr_set_persona_np");
         my_persona_uid = dlsym(h, "posix_spawnattr_set_persona_uid_np");
         my_persona_gid = dlsym(h, "posix_spawnattr_set_persona_gid_np");
+        NSLog(@"persona API: set=%p uid=%p gid=%p", my_persona_set, my_persona_uid, my_persona_gid);
+    } else {
+        NSLog(@"dlopen(NULL) failed");
     }
 }
 
 static int run_as_root(const char *path, char *const argv[]) {
+    NSLog(@"run_as_root: path=%s argv[0]=%s argv[1]=%s", path, argv[0], argv[1] ? argv[1] : "NULL");
     pid_t pid = 0;
     posix_spawnattr_t attr;
     posix_spawnattr_init(&attr);
-    if (my_persona_set) my_persona_set(&attr, 1, 0);
+    if (my_persona_set) {
+        my_persona_set(&attr, 1, 0);
+        NSLog(@"persona_set called");
+    } else {
+        NSLog(@"persona_set NOT available");
+    }
     if (my_persona_uid) my_persona_uid(&attr, 0);
     if (my_persona_gid) my_persona_gid(&attr, 0);
     int r = posix_spawn(&pid, path, NULL, &attr, argv, environ);
     posix_spawnattr_destroy(&attr);
-    if (r != 0) return r;
+    if (r != 0) {
+        NSLog(@"posix_spawn failed: %s (errno=%d)", strerror(r), r);
+        return r;
+    }
+    NSLog(@"posix_spawn OK, pid=%d", pid);
     int status = 0;
     waitpid(pid, &status, 0);
-    return status;
+    int exit_code = WIFEXITED(status) ? WEXITSTATUS(status) : -1;
+    NSLog(@"waitpid done, status=0x%x exited=%d exit_code=%d signaled=%d sig=%d",
+          status, WIFEXITED(status), exit_code, WIFSIGNALED(status), WTERMSIG(status));
+    return exit_code;
 }
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
@@ -42,7 +60,11 @@ static int run_as_root(const char *path, char *const argv[]) {
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    self.window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
+    NSLog(@"AppHideRecovery didFinishLaunchingWithOptions");
+    NSLog(@"uid=%d gid=%d", getuid(), getgid());
+    NSLog(@"jbevasion at /var/jb/usr/bin/jbevasion: %s", access("/var/jb/usr/bin/jbevasion", X_OK) == 0 ? "YES" : "NO");
+    NSLog(@"jbevasion at /usr/bin/jbevasion: %s", access("/usr/bin/jbevasion", X_OK) == 0 ? "YES" : "NO");
+    NSLog(@"stash dir /var/jb/.jbevasion_apphide: %s", access("/var/jb/.jbevasion_apphide", F_OK) == 0 ? "EXISTS" : "NOT FOUND");
     UIViewController *vc = [[UIViewController alloc] init];
     vc.view.backgroundColor = UIColor.systemBackgroundColor;
 
@@ -88,16 +110,24 @@ static int run_as_root(const char *path, char *const argv[]) {
 }
 
 - (void)restoreTapped {
+    NSLog(@"restoreTapped called");
+    const char *jbpath = "/var/jb/usr/bin/jbevasion";
+    if (access(jbpath, X_OK) != 0) {
+        NSLog(@"jbevasion NOT FOUND at %s, trying /usr/bin/jbevasion", jbpath);
+        jbpath = "/usr/bin/jbevasion";
+    }
+    NSLog(@"jbevasion path: %s (accessible=%d)", jbpath, access(jbpath, X_OK) == 0);
+
     UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"恢复中"
         message:@"正在恢复隐藏的应用并重启桌面…"
         preferredStyle:UIAlertControllerStyleAlert];
     [self.window.rootViewController presentViewController:alert animated:YES completion:nil];
 
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        char *showall[] = { "/var/jb/usr/bin/jbevasion", "apphide-showall", NULL };
-        int r1 = run_as_root("/var/jb/usr/bin/jbevasion", showall);
-        char *respring[] = { "/var/jb/usr/bin/jbevasion", "respring", NULL };
-        int r2 = run_as_root("/var/jb/usr/bin/jbevasion", respring);
+        char *showall[] = { (char *)jbpath, "apphide-showall", NULL };
+        int r1 = run_as_root(jbpath, showall);
+        char *respring[] = { (char *)jbpath, "respring", NULL };
+        int r2 = run_as_root(jbpath, respring);
 
         dispatch_async(dispatch_get_main_queue(), ^{
             [alert dismissViewControllerAnimated:YES completion:nil];
@@ -113,14 +143,22 @@ static int run_as_root(const char *path, char *const argv[]) {
 }
 
 - (void)respringTapped {
+    NSLog(@"respringTapped called");
+    const char *jbpath = "/var/jb/usr/bin/jbevasion";
+    if (access(jbpath, X_OK) != 0) {
+        NSLog(@"jbevasion NOT FOUND at %s, trying /usr/bin/jbevasion", jbpath);
+        jbpath = "/usr/bin/jbevasion";
+    }
+    NSLog(@"jbevasion path: %s (accessible=%d)", jbpath, access(jbpath, X_OK) == 0);
+
     UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"重启桌面"
         message:@"正在重启 SpringBoard…"
         preferredStyle:UIAlertControllerStyleAlert];
     [self.window.rootViewController presentViewController:alert animated:YES completion:nil];
 
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        char *args[] = { "/var/jb/usr/bin/jbevasion", "respring", NULL };
-        int r = run_as_root("/var/jb/usr/bin/jbevasion", args);
+        char *args[] = { (char *)jbpath, "respring", NULL };
+        int r = run_as_root(jbpath, args);
 
         dispatch_async(dispatch_get_main_queue(), ^{
             [alert dismissViewControllerAnimated:YES completion:nil];
