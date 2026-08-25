8efd3303f81000c4e011431f9141f1cbfd7974b6
yshen6441 <yshen6441@users.noreply.github.com>
2026-08-25 04:10:58 +0000

    feat: add log terminal output to RecoveryApp with pipe capture of stdout/stderr

--- RecoveryApp/main.m
+++ RecoveryApp/main.m
@@ -3,66 +3,82 @@
 #import <sys/wait.h>
 #import <string.h>
 #import <unistd.h>
+#import <fcntl.h>
 
 extern char **environ;
-
-static int run_cmd(const char *path, char *const argv[]) {
-    pid_t pid = 0;
-    posix_spawnattr_t attr;
-    posix_spawnattr_init(&attr);
-    int r = posix_spawn(&pid, path, NULL, &attr, argv, environ);
-    posix_spawnattr_destroy(&attr);
-    if (r != 0) return r;
-    int status = 0;
-    waitpid(pid, &status, 0);
-    return WIFEXITED(status) ? WEXITSTATUS(status) : -1;
-}
 
 @interface ViewController : UIViewController
 @property (strong, nonatomic) UILabel *statusLabel;
 @property (strong, nonatomic) UIActivityIndicatorView *spinner;
+@property (strong, nonatomic) UITextView *logView;
+- (void)appendLog:(NSString *)msg;
 @end
 
 @implementation ViewController
 
+- (void)appendLog:(NSString *)msg {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.logView.text = [self.logView.text stringByAppendingFormat:@"%@\n", msg];
+        [self.logView scrollRangeToVisible:NSMakeRange(self.logView.text.length, 0)];
+    });
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = UIColor.systemBackgroundColor;
+    CGFloat w = self.view.frame.size.width;
 
-    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(20, 120, self.view.frame.size.width - 40, 40)];
+    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(20, 60, w - 40, 36)];
     title.text = @"越狱环境app隐藏";
     title.textAlignment = NSTextAlignmentCenter;
-    title.font = [UIFont boldSystemFontOfSize:24];
+    title.font = [UIFont boldSystemFontOfSize:22];
     [self.view addSubview:title];
 
-    UILabel *desc = [[UILabel alloc] initWithFrame:CGRectMake(20, 170, self.view.frame.size.width - 40, 40)];
-    desc.text = @"打开开关隐藏所有越狱应用，关闭恢复";
-    desc.numberOfLines = 0;
-    desc.textAlignment = NSTextAlignmentCenter;
-    desc.font = [UIFont systemFontOfSize:15];
-    desc.textColor = UIColor.secondaryLabelColor;
-    [self.view addSubview:desc];
-
-    UISwitch *sw = [[UISwitch alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
-    sw.center = CGPointMake(self.view.frame.size.width / 2, 270);
+    UISwitch *sw = [[UISwitch alloc] init];
+    sw.center = CGPointMake(w / 2, 120);
     sw.onTintColor = UIColor.systemRedColor;
-    sw.transform = CGAffineTransformMakeScale(1.5, 1.5);
+    sw.transform = CGAffineTransformMakeScale(1.3, 1.3);
     [sw addTarget:self action:@selector(switchChanged:) forControlEvents:UIControlEventValueChanged];
     [self.view addSubview:sw];
 
-    self.statusLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 310, self.view.frame.size.width - 40, 30)];
+    self.statusLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 145, w - 40, 24)];
     self.statusLabel.textAlignment = NSTextAlignmentCenter;
-    self.statusLabel.font = [UIFont systemFontOfSize:14];
+    self.statusLabel.font = [UIFont systemFontOfSize:13];
     [self.view addSubview:self.statusLabel];
 
-    self.spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleLarge];
-    self.spinner.center = CGPointMake(self.view.frame.size.width / 2, 380);
+    self.spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleMedium];
+    self.spinner.center = CGPointMake(w / 2, 185);
     self.spinner.hidesWhenStopped = YES;
     [self.view addSubview:self.spinner];
+
+    self.logView = [[UITextView alloc] initWithFrame:CGRectMake(10, 200, w - 20, self.view.frame.size.height - 210)];
+    self.logView.editable = NO;
+    self.logView.font = [UIFont fontWithName:@"Menlo" size:10];
+    self.logView.backgroundColor = UIColor.blackColor;
+    self.logView.textColor = UIColor.greenColor;
+    self.logView.layer.cornerRadius = 8;
+    self.logView.contentInset = UIEdgeInsetsMake(4, 4, 4, 4);
+    [self.view addSubview:self.logView];
+
+    [self appendLog:@"=== AppHide Recovery ==="];
+
+    const char *jbpath = "/var/jb/usr/bin/jbevasion";
+    int acc = access(jbpath, X_OK);
+    [self appendLog:[NSString stringWithFormat:@"jbevasion at %s: %s", jbpath, acc == 0 ? "YES" : "NO"]];
+
+    if (acc != 0) {
+        jbpath = "/usr/bin/jbevasion";
+        acc = access(jbpath, X_OK);
+        [self appendLog:[NSString stringWithFormat:@"jbevasion at %s: %s", jbpath, acc == 0 ? "YES" : "NO"]];
+    }
+
+    [self appendLog:[NSString stringWithFormat:@"uid=%d gid=%d", getuid(), getgid()]];
 
     NSString *stash = @"/var/jb/.apphide-stash";
     NSArray *contents = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:stash error:nil];
     BOOL hasHidden = contents.count > 0;
+    [self appendLog:[NSString stringWithFormat:@"stash dir %@: %ld entries", stash, (long)contents.count]];
+
     sw.on = hasHidden;
     self.statusLabel.text = hasHidden ? @"已隐藏，关闭可恢复" : @"未隐藏，打开可隐藏";
 }
@@ -71,22 +87,77 @@ static int run_cmd(const char *path, char *const argv[]) {
     sender.enabled = NO;
     [self.spinner startAnimating];
     self.statusLabel.text = sender.on ? @"正在隐藏…" : @"正在恢复…";
+    [self appendLog:[NSString stringWithFormat:@"\n>>> %@", sender.on ? @"隐藏所有" : @"恢复所有"]];
 
     const char *jbpath = "/var/jb/usr/bin/jbevasion";
     if (access(jbpath, X_OK) != 0) jbpath = "/usr/bin/jbevasion";
     const char *cmd = sender.on ? "apphide-all" : "apphide-showall";
+    [self appendLog:[NSString stringWithFormat:@"cmd: %s %s", jbpath, cmd]];
 
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        int out_pipe[2], err_pipe[2];
+        pipe(out_pipe);
+        pipe(err_pipe);
+
+        posix_spawn_file_actions_t actions;
+        posix_spawn_file_actions_init(&actions);
+        posix_spawn_file_actions_adddup2(&actions, out_pipe[1], STDOUT_FILENO);
+        posix_spawn_file_actions_adddup2(&actions, err_pipe[1], STDERR_FILENO);
+        posix_spawn_file_actions_addclose(&actions, out_pipe[0]);
+        posix_spawn_file_actions_addclose(&actions, err_pipe[0]);
+
+        pid_t pid = 0;
+        posix_spawnattr_t attr;
+        posix_spawnattr_init(&attr);
         char *const args[] = { (char *)jbpath, (char *)cmd, NULL };
-        int r = run_cmd(jbpath, args);
+        int spawn_rc = posix_spawn(&pid, jbpath, &actions, &attr, args, environ);
+        posix_spawnattr_destroy(&attr);
+        posix_spawn_file_actions_destroy(&actions);
+
+        close(out_pipe[1]);
+        close(err_pipe[1]);
+
+        if (spawn_rc != 0) {
+            [self appendLog:[NSString stringWithFormat:@"posix_spawn failed: %s", strerror(spawn_rc)]];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self.spinner stopAnimating];
+                sender.enabled = YES;
+                self.statusLabel.text = [NSString stringWithFormat:@"spawn失败: %d", spawn_rc];
+                sender.on = !sender.on;
+            });
+            close(out_pipe[0]);
+            close(err_pipe[0]);
+            return;
+        }
+
+        [self appendLog:[NSString stringWithFormat:@"pid=%d", pid]];
+
+        char buf[4096];
+        ssize_t n;
+        while ((n = read(out_pipe[0], buf, sizeof(buf) - 1)) > 0) {
+            buf[n] = '\0';
+            [self appendLog:[NSString stringWithUTF8String:buf]];
+        }
+        while ((n = read(err_pipe[0], buf, sizeof(buf) - 1)) > 0) {
+            buf[n] = '\0';
+            [self appendLog:[NSString stringWithFormat:@"stderr: %s", buf]];
+        }
+        close(out_pipe[0]);
+        close(err_pipe[0]);
+
+        int status = 0;
+        waitpid(pid, &status, 0);
+        int exit_code = WIFEXITED(status) ? WEXITSTATUS(status) : -1;
+        [self appendLog:[NSString stringWithFormat:@"exit code: %d", exit_code]];
 
         dispatch_async(dispatch_get_main_queue(), ^{
             [self.spinner stopAnimating];
             sender.enabled = YES;
-            if (r == 0) {
+            if (exit_code == 0) {
                 self.statusLabel.text = sender.on ? @"已隐藏" : @"已恢复";
+                [self appendLog:@"OK"];
             } else {
-                self.statusLabel.text = [NSString stringWithFormat:@"操作失败: %d", r];
+                self.statusLabel.text = [NSString stringWithFormat:@"操作失败: %d", exit_code];
                 sender.on = !sender.on;
             }
         });
